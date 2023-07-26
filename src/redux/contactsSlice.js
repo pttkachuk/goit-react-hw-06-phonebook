c4ed2fbb24948ba1edbbd74b590b407a1adf67ab
pttkachuk <petrotkachuk6@gmail.com>
2023-07-26 11:35:17 +0200

    upd contactsSlice file

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    contacts: [],
+    contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
 };
 
 const contactsSlice = createSlice({
@@ -10,6 +15,15 @@ const contactsSlice = createSlice({
     reducers: {
         addContacts: (state, action) => {
             state.contacts.push(action.payload);
+        },
+        prepare({ name, number }) {
+            return {
+                payload: {
+                    id: nanoid(),
+                    name,
+                    number,
+                },
+            };
         },
         deleteContacts: (state, action) => {
             state.contacts = state.contacts.filter(({ id }) => id !== action.payload);
